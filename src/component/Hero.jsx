f8c717f3848d81954aebc4288abeb70101d5f285
RiteshPatil7097 <157485127+RiteshPatil7097@users.noreply.github.com>
2025-07-07 10:23:03 +0530

    Add files via upload

---
+++ src/component/Hero.jsx
@@ -0,0 +1,27 @@
+const Hero= () =>{
+    return (
+        <div className="Hero">
+            
+            <div className="Herocontent">
+                <h1>Your Feet Deserve The Best</h1>
+                <p>Your Feet Deserve The Best and We are to help you with youe shoes.Your Feet Deserve The Best and We are to help you with youe shoes.</p>
+                <div className="Herobutton">
+                    <button>Shop now</button>
+                    <button>Category</button>
+                 </div>
+                <div className="shopping">
+                    <p>also Available on</p>
+                    <img src="/public/flipkart.png"></img>
+                    <img src="/public/amazon.png"></img>
+                </div>
+            </div>
+
+            <div className="Heroimage">
+                <img src="/public/shoe_image.png" alt="Shoe Image"></img>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Hero;
---
+++ src/component/Navigation.jsx
@@ -0,0 +1,20 @@
+const Navigation= ()=> {
+    return(
+        <nav>
+            <div className="logo">
+                <img src="/public/Nike_logo.png" alt="logo" style={{height:100}}></img>
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Menu</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+            <button>Login</button>
+            
+          </nav>  
+        )  
+}
+
+
+export default Navigation;
